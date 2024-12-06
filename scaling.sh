60dee59496e16ac1da4014ab2c8df40d160e779f
kangheejuuu <hjrrkd@naver.com>
2024-12-06 17:12:09 +0900

    스크립트 수정

--- scaling.sh
+++ scaling.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
-# 1. 총 슬롯 수 계산
+#/bin/bash
 slots=$(condor_status -compact | awk '{if (NR > 2) print $4}' | grep -E '^[0-9]+$' | awk '{s+=$1} END {print s}')
 
-# 2. 총 작업 수 계산
-jobs=$(condor_q -totals | grep "Total for all users" | awk '{if ($3 ~ /^[0-9]+$/) print $3; else print 0}')
+# 총 작업 수 계산
+jobs=$(condor_q -totals | grep "Total for all users" | awk '{for (i=1; i<=NF; i++) if ($i ~ /^[0-9]+$/) {print $i; exit}}')
 
-# 3. Claimed 노드 목록 계산
+# Claimed 노드 목록 계산
 claimed_nodes=$(condor_status -run | awk '{if(NR>2) print $1}' | tr '\n' ',' | sed 's/,$//')
 
-# 출력
+# Java가 읽을 수 있도록 숫자만 출력
 echo "${slots:-0}"
 echo "${jobs:-0}"
 echo "${claimed_nodes:-none}"
